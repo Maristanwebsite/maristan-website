5bef564dd32b4d1adb60cd52de2bf97e895cd259
Ehsan Qadir <ehsan.qadir@vigorant.com>
2024-03-07 17:30:19 -0800

    Added basepath to images

--- frontend/src/utils/image-resolver.jsx
+++ frontend/src/utils/image-resolver.jsx
@@ -1,6 +1,6 @@
 export const imageResolver = (source) => {
 	const path =
-		'' + source?.data?.attributes?.url;
+		'https://maristan-backend.onrender.com/' + source?.data?.attributes?.url;
 	const width = source?.data?.attributes?.width;
 	const height = source?.data?.attributes?.height;
 	const alt = source?.data?.attributes?.alternativeText;
